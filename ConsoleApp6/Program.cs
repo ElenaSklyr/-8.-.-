5882c6d7acbc59abec0428aa56c8497dd8228306
User <User@WIN-QS9VLSKO6R3>
2024-03-28 22:26:39 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+string mystring = "28";
+/*int number = int.Parse(mystring);
+Console.WriteLine(number.GetType());
+string newstring = number.ToString();
+Console.WriteLine(newstring.GetType());
+Console.WriteLine(newstring);*/
+//byte bt = byte.Parse(mystring);
+//Console.WriteLine(bt.GetType());
+//Console.WriteLine(bt);
+//nint nt = Convert.ToUInt32(bt);
+//Console.WriteLine(nt.GetType());
+//Console.WriteLine(nt);
+
+//int it = int.Parse(mystring);
+//Console.WriteLine(it.GetType());
+//Console.WriteLine(it);
+
+
+
+
+//int integer = Convert.ToInt32(number);
+//Console.WriteLine(number);
+//nint nt = (nint)bt;
+//Console.WriteLine(nt.GetType());
+//int i = (int)bt;
+//Console.WriteLine(bt);
+
+int petr = 5;
+int alex = petr - 4;
+int ivan = alex + 2;
+Console.WriteLine($"Петя взял в библиотеке {petr} книг, Алеша взял {alex} книг, Ваня взял {ivan} книг.");
+Console.WriteLine($"Всего ребята взяли {petr + alex + ivan} книг");
+
+int box1 = 23;
+int box2 = box1 - 11;
+Console.WriteLine($"В корзине всего {box1 + box2} яблок");
---
+++ Тема 8. ДЗ Арифм. операции/Program.cs
@@ -0,0 +1,10 @@
+﻿//Задача 1
+int petr = 5;
+int alex = petr - 4;
+int ivan = alex + 2;
+Console.WriteLine($"Петя взял в библиотеке {petr} книг, Алеша взял {alex} книг, Ваня взял {ivan} книг.");
+Console.WriteLine($"Всего ребята взяли {petr + alex + ivan} книг");
+//Задача 2
+int box1 = 23;
+int box2 = box1 - 11;
+Console.WriteLine($"В корзине всего {box1 + box2} яблок");
