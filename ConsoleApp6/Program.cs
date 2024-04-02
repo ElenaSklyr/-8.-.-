3dc05e242eff13cc67a95c79a83bd85dce53fb69
ElenaSklyr <surjik81@gmail.com>
2024-04-02 21:08:29 +0300

    ДЗ. Арифм.операции

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -1,38 +1,21 @@
-﻿using System;
-
-string mystring = "28";
-/*int number = int.Parse(mystring);
-Console.WriteLine(number.GetType());
-string newstring = number.ToString();
-Console.WriteLine(newstring.GetType());
-Console.WriteLine(newstring);*/
+﻿//using System;
+//Тема 6. Урок 1. ДЗ
+//string mystring = "28";
 //byte bt = byte.Parse(mystring);
 //Console.WriteLine(bt.GetType());
 //Console.WriteLine(bt);
-//nint nt = Convert.ToUInt32(bt);
+//nint nt = bt;
 //Console.WriteLine(nt.GetType());
 //Console.WriteLine(nt);
-
-//int it = int.Parse(mystring);
+//int it = Convert.ToInt32(nt);
 //Console.WriteLine(it.GetType());
 //Console.WriteLine(it);
+//long lg = it;
+//Console.WriteLine(lg.GetType());
+//Console.WriteLine(lg);
+//double db = lg;
+//Console.WriteLine(db.GetType());
+//Console.WriteLine(db);
+//Тема 6. Урок 2. ДЗ
 
 
-
-
-//int integer = Convert.ToInt32(number);
-//Console.WriteLine(number);
-//nint nt = (nint)bt;
-//Console.WriteLine(nt.GetType());
-//int i = (int)bt;
-//Console.WriteLine(bt);
-
-int petr = 5;
-int alex = petr - 4;
-int ivan = alex + 2;
-Console.WriteLine($"Петя взял в библиотеке {petr} книг, Алеша взял {alex} книг, Ваня взял {ivan} книг.");
-Console.WriteLine($"Всего ребята взяли {petr + alex + ivan} книг");
-
-int box1 = 23;
-int box2 = box1 - 11;
-Console.WriteLine($"В корзине всего {box1 + box2} яблок");
--- Тема 8. ДЗ Арифм. операции/Program.cs
+++ Тема 8. ДЗ Арифм. операции/Program.cs
@@ -1,10 +1,24 @@
-﻿//Задача 1
+﻿//Тема 8. Урок1. Задача 1
 int petr = 5;
 int alex = petr - 4;
 int ivan = alex + 2;
 Console.WriteLine($"Петя взял в библиотеке {petr} книг, Алеша взял {alex} книг, Ваня взял {ivan} книг.");
 Console.WriteLine($"Всего ребята взяли {petr + alex + ivan} книг");
-//Задача 2
+//Тема 8. Урок1. Задача 2
 int box1 = 23;
 int box2 = box1 - 11;
 Console.WriteLine($"В корзине всего {box1 + box2} яблок");
+// Тема8. Урок2. Задача 1
+int x = 1*10+3;
+int y = x - 9;
+Console.WriteLine($"Осталось {y} яиц");
+//Тема 8. Урок2. Задача 2
+int x = 25;
+int y = x - 16;
+Console.WriteLine($"Ширина газеты {y} см");
+//Тема 8.Урок2. Задача 3
+int potate = 44;
+int beet = potate / 4;
+int onion = beet / 2;
+Console.WriteLine($"Повар потратил {onion} кг лука");
+
