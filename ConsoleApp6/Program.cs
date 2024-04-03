27b6aa49abf95774010f64a01df5afa099fafaf9
ElenaSklyr <surjik81@gmail.com>
2024-04-03 14:33:43 +0300

    Тема 6. урок 2. дз

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -1,21 +1,27 @@
-﻿//using System;
+﻿using System;
 //Тема 6. Урок 1. ДЗ
-//string mystring = "28";
-//byte bt = byte.Parse(mystring);
-//Console.WriteLine(bt.GetType());
-//Console.WriteLine(bt);
-//nint nt = bt;
-//Console.WriteLine(nt.GetType());
-//Console.WriteLine(nt);
-//int it = Convert.ToInt32(nt);
-//Console.WriteLine(it.GetType());
-//Console.WriteLine(it);
-//long lg = it;
-//Console.WriteLine(lg.GetType());
-//Console.WriteLine(lg);
-//double db = lg;
-//Console.WriteLine(db.GetType());
-//Console.WriteLine(db);
+string mystring = "28";
+byte bt = byte.Parse(mystring);
+Console.WriteLine(bt.GetType());
+Console.WriteLine(bt);
+nint nt = bt;
+Console.WriteLine(nt.GetType());
+Console.WriteLine(nt);
+int it = Convert.ToInt32(nt);
+Console.WriteLine(it.GetType());
+Console.WriteLine(it);
+long lg = it;
+Console.WriteLine(lg.GetType());
+Console.WriteLine(lg);
+double db = lg;
+Console.WriteLine(db.GetType());
+Console.WriteLine(db);
 //Тема 6. Урок 2. ДЗ
+
+int x = 256;
+Console.WriteLine(x.GetType());
+string number = x.ToString();
+Console.WriteLine(number.GetType());
+
 
 
---
+++ Тема 9. Подразрядные операции/Program.cs
@@ -0,0 +1,14 @@
+﻿//1
+Console.WriteLine(8 & 3);
+Console.WriteLine(22 & 4);
+//2
+Console.WriteLine(65 | 29);
+Console.WriteLine(44 | 12);
+//3
+Console.WriteLine(249 ^ 8);
+int n = 249;
+int key = 8;
+int crypt = n ^ key;
+Console.WriteLine($"Зашифрованное сообщение:{crypt}");
+int encrypt = crypt ^ key;
+Console.WriteLine($"Расшифрованное сообщение:{encrypt}");
